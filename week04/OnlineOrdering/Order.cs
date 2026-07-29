8e8b748771fc756d4ea2368f34a5c6fa414d2309
drmukadik-debug <drmukadik@gmail.com>
2026-07-29 06:54:15 +0200

    added new file

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double CalculateTotalCost()
+    {
+        Double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        if(_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+        
+        foreach(Product product in _products)
+        {
+            label += $"{product.GetName()} - ID: {product.GetProductId()}\n";
+        }
+
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Order 1(USA)
+
+        Address address1 = new Address("18 Steven Street","Dallas","Texas", "USA");
+
+        Customer customer1 = new Customer("Allan Walker", address1);
+
+        Order order1 = new Order(customer1);
+
+        order1.AddProduct(new Product("Laptop", "P456", 1200.00, 1));
+        order1.AddProduct(new Product("Wireless Mouse", "P458", 15.35, 3));
+        order1.AddProduct(new Product("Printer", "P459", 250.12, 1));
+
+        // Order 2(Outside USA)
+
+        Address address2 = new Address("13 Prison Street","Mbujimayi","Kasai-East", "DR Congo");
+
+        Customer customer2 = new Customer("Arthur Kabeya", address2);
+
+        Order order2 = new Order(customer2);
+
+        order2.AddProduct(new Product("Ipad", "P460", 250.40, 2));
+        order2.AddProduct(new Product("Smartphone", "P463", 950.75, 2));
+        order2.AddProduct(new Product("Charger", "P465", 16.50, 4));
+
+        // Display Order 1
+
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("             ORDER 1");
+        Console.WriteLine("---------------------------------------");
+        
+        Console.WriteLine("\nPACKING LABEL");
+        Console.WriteLine(order1.GetPackingLabel());
+        
+        Console.WriteLine("SHIPPING LABEL");
+        Console.WriteLine(order1.GetShippingLabel());
+
+        Console.WriteLine($"\nTOTAL PRICE: ${order1.CalculateTotalCost():F2}");
+
+        // Display Order 2
+
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("             ORDER 2");
+        Console.WriteLine("---------------------------------------");
+        
+        Console.WriteLine("\nPACKING LABEL");
+        Console.WriteLine(order2.GetPackingLabel());
+        
+        Console.WriteLine("SHIPPING LABEL");
+        Console.WriteLine(order2.GetShippingLabel());
+
+        Console.WriteLine($"\nTOTAL PRICE: ${order2.CalculateTotalCost():F2}");
+
+
     }
 }
