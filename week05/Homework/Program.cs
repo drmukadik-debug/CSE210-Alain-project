8ff4ab93bd34caa699ae1f24a6555dd50ab53282
drmukadik-debug <drmukadik@gmail.com>
2026-08-05 16:33:16 +0200

    added week05 homework assignments

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        //Test the Assignment class
+        Assignment assignment = new Assignment("Alain MUKADI", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Test the math assignment class
+        MathAssignment math= new MathAssignment("Alain MUKADI", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+        // Test the writing assignment class
+        WritingAssignment writing = new WritingAssignment("Alain MUKADI", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
     }
 }
